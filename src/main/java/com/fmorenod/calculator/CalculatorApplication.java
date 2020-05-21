7c3a53e713ef337acdc2b5606c5d7ed687d3429c
Francisco Moreno <fmorenod@gmail.com>
2020-05-21 13:08:06 -0500

    Caching DNS

--- src/main/java/com/fmorenod/calculator/CalculatorApplication.java
+++ src/main/java/com/fmorenod/calculator/CalculatorApplication.java
@@ -2,15 +2,27 @@ package com.fmorenod.calculator;
 /**
 * Main Spring Application.
 */
+import com.hazelcast.client.config.ClientConfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+
 @SpringBootApplication
+@EnableCaching
+
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
-
+@Bean
+public ClientConfig hazelcastClientConfig() {
+ClientConfig clientConfig = new ClientConfig();
+clientConfig.getNetworkConfig().addAddress("hazelcast");
+return clientConfig;
+}
 }
